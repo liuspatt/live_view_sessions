cf1de8cb94248d33031feaf2ce5f0ad683b6eec0
Luis Patt <liuspatt@gmail.com>
2024-06-16 14:34:52 -0500

    feat(target): add target to js push

--- assets/vendor/SessionStorage.js
+++ assets/vendor/SessionStorage.js
@@ -1,26 +1,31 @@
 const Storage = {
-    mounted() {
-        this.handleEvent("store", (obj) => this.store(obj))
-        this.handleEvent("clear", (obj) => this.clear(obj))
-        this.handleEvent("restore", (obj) => this.restore(obj))
-    },  
+  mounted() {
+      this.handleEvent("store", (obj) => this.store(obj))
+      this.handleEvent("clear", (obj) => this.clear(obj))
+      this.handleEvent("restore", (obj) => this.restore(obj))
+  },  
 
-    store(obj) {
-        console.log("store data")
-        console.log(obj)
-        sessionStorage.setItem(obj.key, obj.data)
-    },
+  store(obj) {
+      sessionStorage.setItem(obj.key, obj.data)
+  },
 
-    restore(obj) {
-      var data = sessionStorage.getItem(obj.key)
-      if (data !== null && obj.key !== null) {
+  restore(obj) {
+    var data = sessionStorage.getItem(obj.key)
+    if (data !== null && obj.key !== null) {
+      // valid if have target 
+      if (obj.target !== null) {
+        this.pushEventTo( obj.target, obj.event, data);
+      }
+      else{
         this.pushEvent(obj.event, data)
       }
-    },
-  
-    clear(obj) {
-      sessionStorage.removeItem(obj.key)
+      
     }
+  },
+
+  clear(obj) {
+    sessionStorage.removeItem(obj.key)
+  }
 }
 
 export default Storage
